099e75918b6452618dfcb25a8e49b861ec0da464
unknown <igorisaias98@gmail.com>
2024-12-07 23:12:35 -0300

    Controller Funcionario

--- src/controllers/funcionarioController.js
+++ src/controllers/funcionarioController.js
@@ -7,6 +7,6 @@ route.post("/", async (request, response) =>{
 
     await funcionario.CreateUsuario(nome, email, cpf, senha, telefone, dt_nascimento, genero , imagem_usuario, nivel_acesso)
 
-    return response.status(201).send({"message": "Diretor cadastrado"})
+    return response.status(201).send({"message": "Funcionario cadastrado"})
 })
 export default route;
